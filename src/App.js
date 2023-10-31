bd6ea3858147f3f39d6a7e3410e522216a85f3e4
Surafels <ssurafel4@gmail.com>
2023-10-31 11:43:23 +0300

    Add routes to link path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/navList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import Home from './components/homePage';
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
-      <Navigation />
+      <Routes>
+        <Route path="homePage" element={<Home />} />
+
+        <Route
+          path="/"
+          element={<FontAwesomeIcon icon={faCog} />}
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/homePage.js
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <div>Surafi</div>
+  );
+}
+
+export default Home;
